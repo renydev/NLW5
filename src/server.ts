aec42a8ee4df08745b75de441575a6150fa6a1de
Renato Ventura <re.infotec@gmail.com>
2021-04-20 18:32:27 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const app = express();
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATCH = Alterar uma informação específica
+ */
+
+app.get("/", (request, response) => {
+  return response.json({message: "Olá NLW 05"})
+});
+
+app.post("/", (request, response) => {
+  return response.json({message: "Usuário salvo com sucesso!"})
+});
+
+app.put("/", (request, response) => {
+  return response.json({message: "Usuário alterado com sucesso!"})
+});
+
+app.delete("/", (request, response) => {
+  return response.json({message: "Usuário excluído com sucesso!"})
+});
+
+app.listen(3333, () => console.log('Server is running on port 3333') );
