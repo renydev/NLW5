a14edb43e686d6c80cf74fcbc555e0f09ab51fdf
Renato Ventura <re.infotec@gmail.com>
2021-04-21 23:21:36 -0300

    Finalização de aula2

--- src/routes.ts
+++ src/routes.ts
@@ -1,34 +1,10 @@
-import { response, Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { Router } from "express";
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
 
-/**
- * Tipos de parametros
- * Route Params => Parametros de rotas
- * http://localhost:3333/settings/1
- * Query Params => Filtros e buscas
- * http://localhost:3333/settings/1?search=algumacoisa
- * Body Params => {
- *  "nome": 'renato',
- *  "idade": 32
- * }
- */
-routes.post("/settings", async (request, response) => {
+const settingsController = new SettingsController();
 
-  const { chat, username } = request.body;
-  const settingsRepository = getCustomRepository(SettingsRepository);
-
-  const settings = settingsRepository.create({
-    chat,
-    username
-  })
-
-  await settingsRepository.save(settings);
-
-  return response.json(settings)
-
-})
+routes.post("/settings", settingsController.create)
 
 export { routes };
